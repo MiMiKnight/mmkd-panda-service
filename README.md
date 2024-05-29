c98e07df14fd0b99c7ea14308faa3c8e3b12e70e
MiMiKnight <victor2015yhm@gmail.com>
2024-05-30 00:01:33 +0800

    [Requirement Number]US202308182020 [Commit Description]配置优化

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ mmkd-panda-service
 ========
 
 mmkd-panda-service project
+
+---
+
+### VM Options
+```text
+-server
+-Duser.language=en
+-Dfile.encoding=UTF-8
+-Duser.timezone=GMT+08:00
+-Dspring.profiles.name=application
+-Dspring.profiles.active=debug
+-Dspring.config.additional-location="F:/ProjectWorkspace/JavaProject/IncubationProject/mmkd-panda-service/deployment/debug/config/"
+```
+
+---
--- service-core/src/main/java/cn/mimiknight/developer/panda/core/Application.java
+++ service-core/src/main/java/cn/mimiknight/developer/panda/core/Application.java
@@ -1,14 +1,22 @@
 package cn.mimiknight.developer.panda.core;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 启动类
  *
  * @author MiMiKnight victor2015yhm@gmail.com
  * @since 2024-05-29 23:12:48
  */
+@Slf4j
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        // 启动项目
+        SpringApplication.run(Application.class, args);
+        log.info("The project started successfully.");
     }
 
 }
