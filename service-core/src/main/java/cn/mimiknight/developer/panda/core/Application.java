7d81b1562207ba027a38cbdde98052ce0255cb3d
MiMiKnight <victor2015yhm@gmail.com>
2024-05-30 07:19:55 +0800

    [Requirement Number]US202308182020 [Commit Description]新增redisson-spring-boot-starter依赖

--- service-core/src/main/java/cn/mimiknight/developer/panda/core/Application.java
+++ service-core/src/main/java/cn/mimiknight/developer/panda/core/Application.java
@@ -1,9 +1,11 @@
 package cn.mimiknight.developer.panda.core;
 
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * 启动类
@@ -16,8 +18,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         // 启动项目
-        SpringApplication.run(Application.class, args);
-        log.info("The project started successfully.");
+        cost(() -> SpringApplication.run(Application.class, args));
+    }
+
+    private static void cost(Runnable function) {
+        Instant start = Instant.now();
+        function.run();
+        Instant end = Instant.now();
+        Duration cost = Duration.between(start, end);
+        log.info("The project started successfully,cost = {}ms", cost.toMillis());
     }
 
 }
