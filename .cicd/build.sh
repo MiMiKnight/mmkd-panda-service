f163fd15c2513c46076d0ac55701d9992982ee0f
MiMiKnight <victor2015yhm@gmail.com>
2024-06-18 08:18:08 +0800

    [Requirement Number]US202308182020 [Commit Description]完善shell

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 # TODO 脚本待完善
+# Maven打包
+# 制作docker镜像
+# 推送docker镜像至镜像仓库
--- deployment/release/bin/start.sh
+++ deployment/release/bin/start.sh
@@ -3,10 +3,13 @@
 # 指定用户
 
 # 当前shell脚本所在绝对路径目录
-current_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd );
+current_dir=$(
+	cd $(dirname ${BASH_SOURCE[0]})
+	pwd
+)
 echo $current_dir
 # 当前shell脚本所在绝对路径目录上一级目录
-current_dir_parent=`dirname "$current_dir"`;
+current_dir_parent=$(dirname "$current_dir")
 
 java -jar $current_dir_parent/lib/@app.jar.name@.jar \
 -server \
@@ -14,3 +17,32 @@ java -jar $current_dir_parent/lib/@app.jar.name@.jar \
 -Dfile.encoding=UTF-8 \
 -Duser.timezone=GMT+04:00 \
 --jasypt.encryptor.password=5177251cc96740fdae33893599768b9e
+
+#===========================================================================================
+# Error Exit
+#===========================================================================================
+error_exit ()
+{
+    echo "ERROR: $1 !!"
+    exit 1
+}
+
+#===========================================================================================
+# Check and Set JAVA_HOME
+#===========================================================================================
+java_home() {
+  [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=$HOME/jdk/java
+  [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/usr/java
+  [ ! -e "$JAVA_HOME/bin/java" ] && JAVA_HOME=/opt/taobao/java
+  [ ! -e "$JAVA_HOME/bin/java" ] && unset JAVA_HOME
+
+  if [ -z "$JAVA_HOME" ]; then
+    JAVA_PATH=$(which java)
+    if [ -z "$JAVA_PATH" ]; then
+      error_exit "Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better!"
+    fi
+    JAVA_HOME=$(dirname "$JAVA_PATH")/..
+    JAVA_HOME=$(cd "$JAVA_HOME" && pwd)
+    export JAVA_HOME
+  fi
+}
